464f0c31131a7901665ad15f248a85b69d3decd4
IvanCCO <ivan.miranda@sptech.school>
2022-05-11 14:29:35 -0300

    Pagina frontal 80%

--- public/script-game.js
+++ public/script-game.js
@@ -1,9 +1,38 @@
+// página inicial (index)
+
 
 // Funçao para fechar a pagina
 function close_tab() {
     if (confirm("Deseja desistir Aluno SPTECH?")) {
       window.close();
     }
-    var customWindow = window.open('', '_blank', '');
-    customWindow.close();
   }
+
+  // Começo da configuração do jogo 
+
+
+  // Setando a variavel como 1 para começar como fácil por default 
+
+
+  var modeVar = 1;
+
+  console.log(modeVar)
+
+
+
+  // Modo normal 
+  if(modeVar == 1){
+
+  }
+
+  // Modo Normal
+  else if (modeVar == 2){
+
+  // Modo dificil
+  }else if (modeVar == 3){
+
+    //CPU-master
+  }else{
+
+  }
+
