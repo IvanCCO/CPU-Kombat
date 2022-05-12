84cebec0740f34b76c650199a70ea79883cb7af0
IvanFre1re <ivanmedeiros0903@outlook.com>
2022-05-11 21:22:28 -0300

    Ïncluindo perguntas

--- public/script-game.js
+++ public/script-game.js
@@ -7,32 +7,4 @@ function close_tab() {
       window.close();
     }
   }
-
-  // Começo da configuração do jogo 
-
-
-  // Setando a variavel como 1 para começar como fácil por default 
-
-
-  var modeVar = 1;
-
-  console.log(modeVar)
-
-
-
-  // Modo normal 
-  if(modeVar == 1){
-
-  }
-
-  // Modo Normal
-  else if (modeVar == 2){
-
-  // Modo dificil
-  }else if (modeVar == 3){
-
-    //CPU-master
-  }else{
-
-  }
-
+  
